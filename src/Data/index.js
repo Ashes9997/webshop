597eb50157f5439859db19f7ab5a2f724785c9e5
Ashes9997 <ayesha.sharif9997@gmail.com>
2024-08-24 17:26:00 -0400

    Slider items including the pictures of products and all related data and their captions are placed in Data folder and imported in home/index.js file .

---
+++ src/Data/index.js
@@ -0,0 +1,54 @@
+export const SliderItems = [
+    {
+      src: 'https://picsum.photos/id/123/1200/400',
+      altText: 'Slide 1',
+      caption: 'Slide 1',
+      key: 1,
+    },
+    {
+      src: 'https://picsum.photos/id/456/1200/400',
+      altText: 'Slide 2',
+      caption: 'Slide 2',
+      key: 2,
+    },
+    {
+      src: 'https://picsum.photos/id/678/1200/400',
+      altText: 'Slide 3',
+      caption: 'Slide 3',
+      key: 3,
+    },
+  ];
+
+
+  export const NewArrivalData = [
+    { name: 'Hand Bag',
+      Description: 'this s a jeans bag with good quality', 
+      images: ["/images/image4.jpg"  ], 
+      price: '2000', 
+      reviews: [],
+    },
+
+    { name: 'Hand Bag',
+        Description: 'this s a jeans bag with good quality', 
+        images: ["/images/image4.jpg"  ], 
+        price: '2000', 
+        reviews: [],
+      },
+
+      { name: 'Hand Bag',
+        Description: 'this s a jeans bag with good quality', 
+        images: ["/images/image4.jpg"  ], 
+        price: '2000', 
+        reviews: [],
+      },
+
+      { name: 'Hand Bag',
+        Description: 'this s a jeans bag with good quality', 
+        images: ["/images/image4.jpg"  ], 
+        price: '2000', 
+        reviews: [],
+      },
+
+    
+    
+  ] 
