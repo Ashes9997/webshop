d3235ced4e7e05bc14cba87c127343df14f2e361
Ashes9997 <ayesha.sharif9997@gmail.com>
2024-08-26 12:08:38 -0400

    react toastify library is installed using "npm i react-toastify" command  and also imported two links in app .js file to raise a toast anywhere from the app

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
 import React from "react";
 import './App.css';
 //mport { BrowserRouter as Router } from 'react-router-dom'; 
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 import { Routes , Route  } from "react-router-dom";
 import Navbar from "./Navbar";
+ import Home from "./Home";
 
-const App = ()  =>{
+const App = () => {
   return (
     <div>
+      <ToastContainer/>
+      
       <Navbar/>
       <Routes>
+        <Route path ="/Home  " element= {<Home />}></Route>
         <Route path ="/Product Details " element= {<div></div>}></Route>
         <Route path ="/Login " element= {<div></div>}></Route>
         <Route path ="/Cart" element= {<div></div>}></Route>
         <Route path ="/Checkout" element= {<div></div>}></Route>
-      </Routes>
+      </Routes> 
      
     </div>
   );
