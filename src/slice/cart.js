ecefa64dfe858d58f14815cc8970d26dcee5eca9
Ashes9997 <ayesha.sharif9997@gmail.com>
2024-08-26 17:45:49 -0400

    functions of Add to Cart and empty cart are added

--- src/slice/cart.js
+++ src/slice/cart.js
@@ -7,39 +7,39 @@ const cartSlice = createSlice({
   initialState: [],
   reducers: {
     addToCart: (state, action) => {
-      state.push({ ...action.payload, quantity: 1 }); // Initialize with quantity 1
+      state.push(action.payload); 
       toast.success("Product added to Cart");
     },
     emptyCart: (state) => {
-      return [];
+      return (state = []);
     },
-    removeFromCart: (state, action) => {
-      return state.filter((item, index) => index !== action.payload); // Remove item by index
-    },
-    increaseQuantity: (state, action) => {
-      const _id = action.payload;
-      const _state = [...state];
-      const index = _state.findIndex((item) => item.id === _id);
-      if (_state[index].quantity + 1 <= 5) {
-        _state[index] = {
-          ..._state[index],
-          quantity: (_state[index].quantity += 1),
-        };
-      }
-    },
-    decreaseQuantity: (state, action) => {
-      const _id = action.payload;
-      const _state = [...state];
-      const index = _state.findIndex((item) => item.id === _id);
-      if (_state[index].quantity - 1 >= 1) {
-        _state[index] = {
-          ..._state[index],
-          quantity: (_state[index].quantity -= 1),
-        };
-      }
-    },
+    // removeFromCart: (state, action) => {
+    //   return state.filter((item, index) => index !== action.payload); // Remove item by index
+    // },
+    // increaseQuantity: (state, action) => {
+    //   const _id = action.payload;
+    //   const _state = [...state];
+    //   const index = _state.findIndex((item) => item.id === _id);
+    //   if (_state[index].quantity + 1 <= 5) {
+    //     _state[index] = {
+    //       ..._state[index],
+    //       quantity: (_state[index].quantity += 1),
+    //     };
+    //   }
+    // },
+    // decreaseQuantity: (state, action) => {
+    //   const _id = action.payload;
+    //   const _state = [...state];
+    //   const index = _state.findIndex((item) => item.id === _id);
+    //   if (_state[index].quantity - 1 >= 1) {
+    //     _state[index] = {
+    //       ..._state[index],
+    //       quantity: (_state[index].quantity -= 1),
+    //     };
+    //   }
+    // },
   },
 });
 
-export const { addToCart, emptyCart, removeFromCart, increaseQuantity, decreaseQuantity } = cartSlice.actions;
+export const { addToCart, emptyCart} = cartSlice.actions;
 export default cartSlice.reducer;
