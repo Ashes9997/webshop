b66e1749c8ac5182f0e2c9e2372a526af8fc59e0
Ashes9997 <ayesha.sharif9997@gmail.com>
2024-08-26 17:40:02 -0400

    details of changes are given below .
    1- react-toastify library is downloaded
    2-  i made function MyNavBar in navbar componnet but i was exporting it as navbar .i changed it to MyNavBar
     3-

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import React from "react";
 import './App.css';
 //mport { BrowserRouter as Router } from 'react-router-dom'; 
-import { ToastContainer, toast } from 'react-toastify';
+import { ToastContainer} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 import { Routes , Route  } from "react-router-dom";
-import Navbar from "./Navbar";
+import MyNavBar from "./Navbar";
  import Home from "./Home";
+ import Cart from "./Cart";
+ import Login from "./Login";
 
 const App = () => {
   return (
     <div>
       <ToastContainer/>
       
-      <Navbar/>
+      <MyNavBar/>
       <Routes>
+        <Route path ="/ " element= {<Home />}></Route>
         <Route path ="/Home  " element= {<Home />}></Route>
         <Route path ="/Product Details " element= {<div></div>}></Route>
-        <Route path ="/Login " element= {<div></div>}></Route>
-        <Route path ="/Cart" element= {<div></div>}></Route>
+        <Route path ="/Login " element= {<Login />}></Route>
+        <Route path ="/Cart" element= {<Cart />}></Route>
         <Route path ="/Checkout" element= {<div></div>}></Route>
       </Routes> 
      
